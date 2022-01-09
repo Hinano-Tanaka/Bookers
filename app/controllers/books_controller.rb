d604ad3693cedd226ce30dd48ee705cbb99fad6c
EC2 Default User <ec2-user@ip-172-31-20-133.ec2.internal>
2022-01-09 05:36:15 +0000

    [Update] Added destroy and update actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,25 +4,36 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created'
+      redirect_to show_book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
-    @book = book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to show_book_path(@book.id)
+  end
+
   def destroy
-    book = book.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get 'books' => 'books#new'
+  # get 'books' => 'books#new'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  delete 'books' => 'books#destroy'
+  get 'books' => 'books#index', as: 'index_book'
+  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # get 'homes/top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
