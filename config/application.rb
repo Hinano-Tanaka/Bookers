9a9d74908f79885e11ae0604337d67a31c86c361
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-02-04 03:55:43 +0000

    バッチ処理を学ぼう

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Bookers
   class Application < Rails::Application
+    config.paths.add 'lib', eager_load: true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
